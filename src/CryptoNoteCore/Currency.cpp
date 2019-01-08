8a86584c96a989ac5b570a0135de8e28725548e0
andom <46446842+enaqfa@users.noreply.github.com>
2019-01-08 11:17:07 -0500

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -103,6 +103,23 @@ bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64
 
   uint64_t baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor;
 
+         if (alreadyGeneratedCoins == 0) {
+            baseReward = 10;
+        }
+
+        if (alreadyGeneratedCoins == 500) {
+            baseReward =m_moneySupply*1000000000;
+        }
+  
+        if (alreadyGeneratedCoins == 500000000000000) {
+            baseReward =m_moneySupply*0.10;
+        }
+
+     if (alreadyGeneratedCoins + baseReward >= m_moneySupply) {
+                baseReward = 0;
+            }
+
+  
   medianSize = std::max(medianSize, m_blockGrantedFullRewardZone);
   if (currentBlockSize > UINT64_C(2) * medianSize) {
     logger(TRACE) << "Block cumulative size is too big: " << currentBlockSize << ", expected less than " << 2 * medianSize;
